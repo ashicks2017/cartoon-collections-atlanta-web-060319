29de15eaa8831b1e2a7aa7a92e6ed89b7ce168bb
aeisha simmons <ashicks2017@outlook.com>
2019-05-19 22:30:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
-
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "Sleepy", "Happy", "Sneezy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+assorted_words = ["two", "go", "industrious", "bop"]
+call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
+short_words = ["puff", "go", "two"]
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
